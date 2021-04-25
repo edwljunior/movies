3138bb8d687b485638b46d50be486bd23709a40a
edinho.029 <edinho.029@gmail.com>
2021-04-25 19:01:47 -0300

    fix: Corrige armazenamento da lista de filmes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,35 +2,32 @@ import { request, response, Router } from 'express';
 import axios from 'axios';
 
 const routes = Router();
-let lista: any[] = [];
-let listaAno: any[] = [];
 
 
-routes.get('/', async (request,response) => {
+routes.get('/api/movies/count', async (request, response) => {
+  let lista: any[] = [];
   let myMap = new Map();
   let link;
   let contPage = 1;
-
+  let listaAno: any[] = [];
   do {
-    console.log(contPage);
     link = await axios.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${request.query.Title}&page=${contPage++}`);
     lista = [...lista, ...link.data.data]
   } while (contPage <= link.data.total_pages);
 
   lista.forEach((value, key) => {
-    if(myMap.has(value.Year)){
-      myMap.set(value.Year,myMap.get(value.Year)+1)
+    if (myMap.has(value.Year)) {
+      myMap.set(value.Year, myMap.get(value.Year) + 1)
     }
-    else{
-      myMap.set(value.Year,1)
+    else {
+      myMap.set(value.Year, 1)
     }
   })
 
-  myMap.forEach((value,key) => {
-    listaAno.push({year:key,movie:value});
+  myMap.forEach((value, key) => {
+    listaAno.push({ year: key, movie: value });
   });
-  console.log({'movesByYear':listaAno,total:link.data.total});
-  response.json({'movesByYear':listaAno,total:link.data.total});
+  response.json({ 'movesByYear': listaAno, total: link.data.total });
 });
 
 
